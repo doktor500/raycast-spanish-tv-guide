d2710e19245933ded6c2b4b29592372cf16f3dfd
David Molinero <doktor500@users.noreply.github.com>
2023-10-03 16:44:01 +0200

    Create all channels view

--- src/index.tsx
+++ src/index.tsx
@@ -1,64 +1,43 @@
-import { List, showToast, Toast } from "@raycast/api";
-import { useEffect, useReducer } from "react";
+import { showToast, Toast } from "@raycast/api";
+import React, { useEffect, useReducer } from "react";
 import Jimp from "jimp";
 
-import { ChannelSchedule, TVSchedule } from "./modules/tv/domain/tvSchedule";
-import ChannelDetails from "./components/ChannelDetails";
+import { TVSchedule } from "./modules/tv/domain/tvSchedule";
 import { tvScheduleRepository } from "./modules/tv/repositories/tvScheduleRepository";
-import { isEmpty, isNull } from "./utils/objectUtils";
-import ErrorMessage from "./components/ErrorMessage";
+import ErrorMessage from "./views/ErrorMessage";
+import { AllChannels } from "./views/AllChannelsView";
 
-type State = {
+export type State = {
   tvSchedule: TVSchedule;
   isShowingDetail: boolean;
   iconsLoaded: boolean;
   error?: Error;
   hoveredChannel?: string;
+  selectedChannel?: string;
 };
 
 const ICONS_DIRECTORY = "/tmp/raycast/spanish-tv-guide/icons";
 export const ERROR_MESSAGE = "Error fetching TV guide";
+export const iconPath = (icon: string) => `${ICONS_DIRECTORY}/${iconName(icon)}`;
 
 const initialState: State = { tvSchedule: [], isShowingDetail: false, iconsLoaded: false };
 const reducer = (state: State, newState: Partial<State>) => ({ ...state, ...newState });
 
 const Command = () => {
   const [state, setState] = useReducer(reducer, initialState);
-  const { tvSchedule, isShowingDetail, iconsLoaded, error, hoveredChannel } = state;
+  const { tvSchedule, error } = state;
   const initialize = () => tvScheduleRepository.getAll().then((tvSchedule) => setState({ tvSchedule }));
 
   useEffect(() => void initialize().catch((error) => setState({ error })), []);
   useEffect(() => void generateIcons(tvSchedule).then(() => setState({ iconsLoaded: true })), [tvSchedule]);
   useEffect(() => error && void showToast({ style: Toast.Style.Failure, title: ERROR_MESSAGE }), [error]);
 
-  const selectChannel = (channel: string | null) => {
-    const channelSelected = !isNull(channel);
-    if (channelSelected) setState({ hoveredChannel: channel });
-    setState({ isShowingDetail: channelSelected });
-  };
-
   if (error) return <ErrorMessage />;
-
-  return (
-    <List
-      isLoading={isEmpty(tvSchedule) || !iconsLoaded}
-      selectedItemId={hoveredChannel}
-      isShowingDetail={isShowingDetail}
-      onSelectionChange={selectChannel}
-    >
-      {tvSchedule.map(renderChannel)}
-    </List>
-  );
-};
-
-const renderChannel = ({ icon, name, schedule }: ChannelSchedule) => {
-  const detail = <ChannelDetails name={name} schedule={schedule} icon={icon} />;
-  return <List.Item key={name} title={name} detail={detail} icon={iconPath(icon)} />;
+  return <AllChannels state={state} setState={setState} />;
 };
 
 const generateIcons = (tvSchedule: TVSchedule) => Promise.all(tvSchedule.map(({ icon }) => generateIcon(icon)));
 const generateIcon = (icon: string) => Jimp.read(icon).then((image) => image.contain(256, 256).write(iconPath(icon)));
-const iconPath = (icon: string) => `${ICONS_DIRECTORY}/${iconName(icon)}`;
 const iconName = (icon: string) => icon.substring(icon.lastIndexOf("/") + 1);
 
 export default Command;
---
+++ src/views/AllChannelsView.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { isEmpty, isNull } from "../utils/objectUtils";
+import { ChannelSchedule } from "../modules/tv/domain/tvSchedule";
+import { Action, ActionPanel, List } from "@raycast/api";
+import { iconPath, State } from "../index";
+import ChannelDetails from "../components/ChannelDetails";
+
+export const AllChannels = ({ state, setState }: { state: State; setState: React.Dispatch<Partial<State>> }) => {
+  const SELECT_CHANNEL_ACTION = "Select Channel";
+  const { tvSchedule, isShowingDetail, iconsLoaded, hoveredChannel } = state;
+
+  const selectChannel = (channel: string | null) => {
+    const channelSelected = !isNull(channel);
+    if (channelSelected) setState({ hoveredChannel: channel });
+    setState({ isShowingDetail: channelSelected });
+  };
+
+  const renderChannel = ({ icon, name, schedule }: ChannelSchedule) => {
+    const detail = <ChannelDetails name={name} schedule={schedule} icon={icon} />;
+    return (
+      <List.Item
+        key={name}
+        title={name}
+        detail={detail}
+        icon={iconPath(icon)}
+        actions={
+          <ActionPanel>
+            <Action title={SELECT_CHANNEL_ACTION} onAction={() => setState({ selectedChannel: name })} />
+          </ActionPanel>
+        }
+      />
+    );
+  };
+
+  return (
+    <List
+      isLoading={isEmpty(tvSchedule) || !iconsLoaded}
+      selectedItemId={hoveredChannel}
+      isShowingDetail={isShowingDetail}
+      onSelectionChange={selectChannel}
+    >
+      {tvSchedule.map(renderChannel)}
+    </List>
+  );
+};
