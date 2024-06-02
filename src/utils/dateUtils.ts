4c8f308c0d53079d039ac49551b64a2fa3b10774
David Molinero <doktor500@users.noreply.github.com>
2024-06-02 12:10:23 +0100

    Fix time issue

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -6,7 +6,7 @@ const oneDay = Duration.fromObject({ day: 1 });
 const currentDate = () => now().toISOString().substring(0, 11);
 
 const getTime = (date: Date) => DateTime.fromJSDate(date).setZone(TIME_ZONE).toLocaleString(DateTime.TIME_24_SIMPLE);
-const now = () => DateTime.now().setZone(TIME_ZONE).toJSDate();
+const now = () => DateTime.now().setZone(TIME_ZONE, { keepLocalTime: true }).toJSDate();
 const parseTime = (time: string) => DateTime.fromISO(`${currentDate()}${time}`).setZone(TIME_ZONE, { keepLocalTime: true }).toJSDate();
 const plusOneDay = (date: Date) => DateTime.fromJSDate(date).setZone(TIME_ZONE, { keepLocalTime: true }).plus(oneDay).toJSDate();
 
--- test/utils/dateUtils.spec.ts
+++ test/utils/dateUtils.spec.ts
@@ -3,7 +3,7 @@ import { getTime, parseTime } from "../../src/utils/dateUtils";
 process.env.TZ = "UTC";
 
 describe("date utils", () => {
-    beforeEach(() => jest.useFakeTimers().setSystemTime(new Date("2023-01-01")));
+    beforeEach(() => jest.useFakeTimers().setSystemTime(new Date("2023-01-01:12:00")));
 
     it("returns date's hours and minutes", () => {
         expect(getTime(new Date("2023-01-29T16:40:45.280Z"))).toEqual("17:40");
