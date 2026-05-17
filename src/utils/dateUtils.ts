e94f7c0745da9e7fcee56e83bd839e6cdf0b7bf1
David Molinero <doktor500@users.noreply.github.com>
2026-05-17 10:19:43 +0100

    Pull contributions

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,9 +1,22 @@
-import { DateTime, Interval } from "luxon";
+import { DateTime, SystemZone } from "luxon";
+import { isString } from "./stringUtils";
 
-const TIME_ZONE = "Europe/Madrid";
+const ISO_DATE_PATTERN = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d{3})?Z?$/;
+const systemZone = SystemZone.instance;
 
-const parseDate = (date: string) => DateTime.fromISO(date, { zone: TIME_ZONE }).toJSDate();
-const getTime = (date: Date) => DateTime.fromJSDate(date).toLocaleString(DateTime.TIME_24_SIMPLE);
-const isActiveInterval = (from: Date, to: Date) => Interval.fromDateTimes(from, to).contains(DateTime.now());
+const now = () => DateTime.now().setZone(systemZone).toJSDate();
 
-export { getTime, isActiveInterval, parseDate };
+const getTime = (date: Date) => {
+  return DateTime.fromJSDate(date, { zone: systemZone.name }).toLocaleString(DateTime.TIME_24_SIMPLE);
+};
+
+const dateReviver = (_: string, value: unknown) => {
+  if (isSerializedISODate(value)) {
+    return DateTime.fromISO(value).toJSDate();
+  }
+  return value;
+};
+
+const isSerializedISODate = (value: unknown): value is string => isString(value) && ISO_DATE_PATTERN.test(value);
+
+export { dateReviver, getTime, now };
